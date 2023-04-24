b05f055d09e39656c17d06114ab3fee1a9b94eb4
Jesper Alcala <jesperdalcala@gmail.com>
2023-04-24 22:42:48 +0800

    fetch data from RTDB, fix typos etc.

--- getNpay-client/src/adminpage/AddProduct.js
+++ getNpay-client/src/adminpage/AddProduct.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { addProductToFirestore } from "../redux/getNpaySlice";
+import { getDatabase, ref, onValue} from "firebase/database";
+import { app } from "../firebase.config";
 
 const AddProduct = () => {
   const dispatch = useDispatch();
 
   //ADD PRODUCT
   const [productName, setProductName] = useState("");
-  const [RFIDnum, setRFIDnum] = useState("");
+  const [RFIDnum, setRFIDnum] = useState([]);
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [imageproduct, setImageProduct] = useState("");
@@ -25,7 +27,7 @@ const AddProduct = () => {
     //dispatch function
     dispatch(addProductToFirestore(product));
     setProductName("");
-    setRFIDnum("");
+    setRFIDnum([]);
     setPrice("");
     setCategory("");
     setImageProduct("");
@@ -33,32 +35,51 @@ const AddProduct = () => {
   //Edit Products
   const [productEdit, setProductEdit] = useState(null);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    const db = getDatabase(app);
+    const rfidRef = ref(db, "UsersData/cLmwoz9mYfeVQv9u2qdlskMplRy1/data_uploads/rfidtag_id");
+    onValue(rfidRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log(data); // Add this line to log the data
+      if (data) {
+        const rfidTags = Object.values(data);
+        setRFIDnum(rfidTags);
+      } else {
+        setRFIDnum([]);
+      }
+    });
+  };
+
   return (
     <div
       id="AddProductModal"
-      tabindex="-1"
+      tabIndex="-1"
       aria-hidden="true"
-      class="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      className="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
     >
-      <div class="relative w-full max-w-2xl max-h-full">
+      <div className="relative w-full max-w-2xl max-h-full">
         {/* <!-- Modal content --> */}
         <form
           onSubmit={handleAddProduct}
-          class="relative bg-white rounded-lg shadow dark:bg-gray-700"
+          className="relative bg-white rounded-lg shadow dark:bg-gray-700"
         >
           {/* <!-- Modal header --> */}
-          <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+          <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
               Add Product
             </h3>
             <button
               type="button"
-              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-hide="editProductModal"
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-hide="AddProductModal"
             >
               <svg
                 aria-hidden="true"
-                class="w-5 h-5"
+                className="w-5 h-5"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -72,12 +93,12 @@ const AddProduct = () => {
             </button>
           </div>
           {/* <!-- Modal body --> */}
-          <div class="p-6 space-y-6">
-            <div class="grid grid-cols-6 gap-6">
-              <div class="col-span-6 sm:col-span-3">
+          <div className="p-6 space-y-6">
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   for="product-name"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Product Name
                 </label>
@@ -87,52 +108,47 @@ const AddProduct = () => {
                   type="text"
                   name="product-name"
                   id="product-name"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="e.g Mega Sardines"
-                  required=""
+                  required
                 />
               </div>
-              <div class="col-span-6 sm:col-span-3">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   for="RFIDtagNum"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   RFID Tag UID No.:
                 </label>
-                <input
-                  onChange={(e) => setRFIDnum(e.target.value)}
-                  value={RFIDnum}
-                  type="number"
-                  name="RFIDtagNum"
-                  id="RFIDtagNum"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="e.g 1234"
-                  required=""
-                />
+                <ul className="list-disc pl-5">
+                  {RFIDnum.map((rfidTag, index) => (
+                    <li key={index}>{rfidTag}</li>
+                  ))}
+                </ul>
               </div>
 
-              <div class="col-span-6 sm:col-span-3">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   for="price"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Price
                 </label>
                 <input
                   onChange={(e) => setPrice(e.target.value)}
                   value={price}
-                  type="price"
+                  type="number"
                   name="price"
                   id="price"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="e.g. ₱5.00"
-                  required=""
+                  required
                 />
               </div>
-              <div class="col-span-6 sm:col-span-3">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   for="category"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Category
                 </label>
@@ -142,28 +158,28 @@ const AddProduct = () => {
                   type="text"
                   name="category"
                   id="category"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="e.g Canned Goods"
-                  required=""
+                  required
                 />
               </div>
-              <div class="col-span-6 sm:col-span-3">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   for="company"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Upload Image
                 </label>
 
-                <div class="flex items-center justify-center w-full">
+                <div className="flex items-center justify-center w-full">
                   <label
                     for="dropzone-file"
-                    class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                    className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
                   >
-                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
                       <svg
                         aria-hidden="true"
-                        class="w-10 h-10 mb-3 text-gray-400"
+                        className="w-10 h-10 mb-3 text-gray-400"
                         fill="none"
                         stroke="currentColor"
                         viewBox="0 0 24 24"
@@ -176,11 +192,11 @@ const AddProduct = () => {
                           d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
                         ></path>
                       </svg>
-                      <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                        <span class="font-semibold">Click to upload</span> or
+                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                        <span className="font-semibold">Click to upload</span> or
                         drag and drop
                       </p>
-                      <p class="text-xs text-gray-500 dark:text-gray-400">
+                      <p className="text-xs text-gray-500 dark:text-gray-400">
                         SVG, PNG, JPG or GIF (MAX. 800x400px)
                       </p>
                     </div>
@@ -189,7 +205,7 @@ const AddProduct = () => {
                       value={imageproduct}
                       id="dropzone-file"
                       type="file"
-                      class="hidden"
+                      className="hidden"
                     />
                   </label>
                 </div>
@@ -197,10 +213,10 @@ const AddProduct = () => {
             </div>
           </div>
           {/* <!-- Modal footer --> */}
-          <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+          <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
             <button
               type="submit"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Save all
             </button>
--- getNpay-client/src/adminpage/EditProduct.js
+++ getNpay-client/src/adminpage/EditProduct.js
@@ -42,29 +42,29 @@ const EditProduct = () => {
   return (
     <div
       id="editProductModal"
-      tabindex="-1"
+      tabIndex="-1"
       aria-hidden="true"
-      class="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
+      className="fixed top-0 left-0 right-0 z-50 items-center justify-center hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full"
     >
-      <div class="relative w-full max-w-2xl max-h-full">
+      <div className="relative w-full max-w-2xl max-h-full">
         {/* <!-- Modal content --> */}
         <form
           onSubmit={handleUpdateProduct}
-          class="relative bg-white rounded-lg shadow dark:bg-gray-700"
+          className="relative bg-white rounded-lg shadow dark:bg-gray-700"
         >
           {/* <!-- Modal header --> */}
-          <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+          <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+            <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
               Edit Product
             </h3>
             <button
               type="button"
-              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
               data-modal-hide="editProductModal"
             >
               <svg
                 aria-hidden="true"
-                class="w-5 h-5"
+                className="w-5 h-5"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -78,12 +78,12 @@ const EditProduct = () => {
             </button>
           </div>
           {/* <!-- Modal body --> */}
-          <div class="p-6 space-y-6">
-            <div class="grid grid-cols-6 gap-6">
-              <div class="col-span-6 sm:col-span-3">
+          <div className="p-6 space-y-6">
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   for="product-name"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Product Name
                 </label>
@@ -93,15 +93,15 @@ const EditProduct = () => {
                   type="text"
                   name="product-name"
                   id="product-name"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="e.g Mega Sardines"
                   required=""
                 />
               </div>
-              <div class="col-span-6 sm:col-span-3">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   for="RFIDtagNum"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   RFID Tag UID No.:
                 </label>
@@ -111,16 +111,16 @@ const EditProduct = () => {
                   type="number"
                   name="RFIDtagNum"
                   id="RFIDtagNum"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="e.g 1234"
                   required=""
                 />
               </div>
 
-              <div class="col-span-6 sm:col-span-3">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   for="price"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Price
                 </label>
@@ -130,15 +130,15 @@ const EditProduct = () => {
                   type="price"
                   name="price"
                   id="price"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="e.g. ₱5.00"
                   required=""
                 />
               </div>
-              <div class="col-span-6 sm:col-span-3">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   for="category"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Category
                 </label>
@@ -148,28 +148,28 @@ const EditProduct = () => {
                   type="text"
                   name="category"
                   id="category"
-                  class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="e.g Canned Goods"
                   required=""
                 />
               </div>
-              <div class="col-span-6 sm:col-span-3">
+              <div className="col-span-6 sm:col-span-3">
                 <label
                   for="company"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Upload Image
                 </label>
 
-                <div class="flex items-center justify-center w-full">
+                <div className="flex items-center justify-center w-full">
                   <label
                     for="dropzone-file"
-                    class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                    className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
                   >
-                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
                       <svg
                         aria-hidden="true"
-                        class="w-10 h-10 mb-3 text-gray-400"
+                        className="w-10 h-10 mb-3 text-gray-400"
                         fill="none"
                         stroke="currentColor"
                         viewBox="0 0 24 24"
@@ -182,11 +182,11 @@ const EditProduct = () => {
                           d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
                         ></path>
                       </svg>
-                      <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                        <span class="font-semibold">Click to upload</span> or
+                      <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                        <span className="font-semibold">Click to upload</span> or
                         drag and drop
                       </p>
-                      <p class="text-xs text-gray-500 dark:text-gray-400">
+                      <p className="text-xs text-gray-500 dark:text-gray-400">
                         SVG, PNG, JPG or GIF (MAX. 800x400px)
                       </p>
                     </div>
@@ -195,7 +195,7 @@ const EditProduct = () => {
                       value={editedimageproduct}
                       id="dropzone-file"
                       type="file"
-                      class="hidden"
+                      className="hidden"
                     />
                   </label>
                 </div>
@@ -203,10 +203,10 @@ const EditProduct = () => {
             </div>
           </div>
           {/* <!-- Modal footer --> */}
-          <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+          <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
             <button
               type="submit"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Save all
             </button>
